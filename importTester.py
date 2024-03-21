caf6c68cb069f4a602368db2ac1da349cca9ed18
angadsingh21 <angadsingh5@ontariotechu.net>
2024-03-21 07:00:30 -0400

    cleanup

--- importTester.py
+++
@@ -1,8 +0,0 @@
-# Try importing the authenticate function
-try:
-    # Import the function directly from the file path
-    from src.client.client import authenticate
-
-    print("Import successful!")
-except ModuleNotFoundError as e:
-    print(f"Import error: {e}")
