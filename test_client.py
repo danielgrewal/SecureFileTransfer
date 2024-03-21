916e7aa0abd57e6eae3fe47f76721e949aec0c95
angadsingh21 <angadsingh5@ontariotechu.net>
2024-03-21 07:26:28 -0400

    valid creds testing

--- test_client.py
+++ test_client.py
@@ -4,12 +4,15 @@ from src.client import client
 def test_invalid_creds():
    """Tests that authentication fails with invalid credentials and that an appropriate error message is displayed."""
 
-   username = input("Enter username: ")
-   password = input("Enter password: ")
+   print("TESTING AUTH")
 
-   # Attempt to authenticate with provided credentials
+   username = "angad"
+   password = "newpass"
+
+   
    with pytest.raises(Exception) as e:  # Expect an exception on failed authentication
-       result = client.authenticate(username, password)
+       result = client.authenticate(username, password) # Attempt to authenticate with provided credentials
+       print(result)
 
    # Assert that the error message indicates invalid credentials
    assert "Invalid authentication credentials." in client.errorMsg
