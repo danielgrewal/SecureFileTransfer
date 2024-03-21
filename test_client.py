b550f0edbee3978b0d6c5ad367d008d0c8a304ed
angadsingh21 <angadsingh5@ontariotechu.net>
2024-03-21 13:08:13 -0400

    verification test added

--- test_client.py
+++ test_client.py
@@ -1,19 +1,78 @@
 import pytest
+from uuid import uuid4
 from src.client import client
 
-def test_invalid_creds():
-   """Tests that authentication fails with invalid credentials and that an appropriate error message is displayed."""
+@pytest.mark.asyncio
+async def async_authenticate(username, password): # async wrapper
+    print("Async is being called")
+    return client.authenticate(username, password)
 
-   print("TESTING AUTH")
+@pytest.mark.asyncio
+async def test_invalid_auth():
+    
+    print("TESTING AUTH WITH INVALID CREDS")
 
-   username = "angad"
-   password = "newpass"
+    username = "angad"
+    password = "newpass"
 
-   
-   with pytest.raises(Exception) as e:  # Expect an exception on failed authentication
-       result = client.authenticate(username, password) # Attempt to authenticate with provided credentials
-       print(result)
+    try:
+        # Attempt to authenticate with provided credentials
+        access_token, errorMsg = await async_authenticate(username, password)
 
-   # Assert that the error message indicates invalid credentials
-   assert "Invalid authentication credentials." in client.errorMsg
+        if access_token is not None:
+            print("\nAuthentication successful!")
+            print("Access Token: ", access_token)
 
+    except Exception as e:
+        # Assert the error message indicates invalid credentials
+        assert "Invalid authentication credentials." in str(e)
+        print("\nAuthentication failed:", e)
+
+@pytest.mark.asyncio
+async def test_valid_auth():
+
+    """Tests that authentication fails with invalid credentials and that an appropriate error message is displayed."""
+    
+    print("TESTING AUTH WITH VALID CREDS")
+
+    username = "admin"
+    password = "root"
+
+    try:
+        # Attempt to authenticate with provided credentials
+        access_token, errorMsg = await async_authenticate(username, password)
+
+        if access_token is not None:
+            print("\nAuthentication successful!")
+            print("Access Token: ", access_token)
+
+    except Exception as e:
+        # Assert the error message indicates invalid credentials
+        assert "Invalid authentication credentials." in str(e)
+        print("\nAuthentication failed:", e)
+
+@pytest.mark.asyncio
+async def test_valid_verification():
+
+  username = "usman"
+  password = "pass"  
+
+  access_token = await async_authenticate(username, password)
+
+  headers = {"Accept": "application/json",
+               "Authorization": f"Bearer {access_token}"}
+
+  assert client.is_valid_responder(username, headers) is True
+
+@pytest.mark.asyncio
+
+async def test_invalid_verification():
+
+  username = "usman"  
+
+  access_token = uuid4() # generating unique token
+  
+  headers = {"Accept": "application/json",
+               "Authorization": f"Bearer {access_token}"}
+
+  assert client.is_valid_responder(username, headers) is False
