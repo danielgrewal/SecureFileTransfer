b443a2a4fcb98bfaf6de649800c8d47b9422e397
angadsingh21 <angadsingh5@ontariotechu.net>
2024-03-23 00:18:23 -0400

    fun commit

--- test_server.py
+++ test_server.py
@@ -1,17 +1,19 @@
 # from uuid import uuid4
 # import requests
 
-# def test_endSession(session_id: str, headers):
+# def test_endSession():
+
+#     session_id = 3
+        
+#     access_token = uuid4() # generating fake token
+        
+#     headers = {"Accept": "application/json",
+#                 "Authorization": f"Bearer {access_token}"}
+    
 #     SERVER_URL_BASE = "https://localhost"
 #     response = requests.post(f'{SERVER_URL_BASE}/endsession',
 #                              json={"session_id": session_id}, headers=headers, verify=False)
 
 #     print(response.json().get("status"))
-#     return (response.json().get("status") == "Success")
 
-# access_token = uuid4() # generating fake token
-    
-# headers = {"Accept": "application/json",
-#             "Authorization": f"Bearer {access_token}"}
-
-# print(test_endSession(3, headers))
+#     assert response.json().get("status") is not "Success"
